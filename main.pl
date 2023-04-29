caeffc2d501f49c20e54eeee9a40effe62a6e9fa
wildbartty <wildbartty@gmail.com>
2023-04-29 22:48:09 +1000

    final perl commit

--- main.pl
+++ main.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use diagnostics;
 use Config::IniFiles;
 use Data::Dumper;
 
@@ -39,19 +40,49 @@ sub read_world {
     return @ret;
 }
 
-sub parse_atom {
-#TODO: write specific parsers for ut2004 and xfce4 and amazon-ec2 r10k cli53 
-    my %ret;
-    my ($atom) = @_;
-    if ($atom =~ /(.+)\/(.+)/) {
-        print "$atom\t$1\t$2\n"
-    };
+sub split_version {
+    #loop in repo folders til you get a package that matches
+    my $pkg = $_[0];
+    my $class = $_[1];
+    if (defined $class) {
+        print("Got class\t","$pkg $class\n");
+    } 
+    else {
+        print("Ain't no fashon on those hoes\t","pkg: $pkg class: $class\n");
+    }
 }
 
-my @x = read_world;
-@x = (
+sub parse_atom {
+#TODO: write specific parsers for ut2004 and xfce4 and amazon-ec2 r10k cli53 
+    #Parsed atom format is [name, class, version, operator, slot] in
+    #an array
+    my @ret = ("","","","","");
+    my $atom = $_[0];
+    my @split_slot = split(/:/, $atom);
+    my @split_class = split(/\//, $split_slot[0]);
+    
+    my ($oper, $class);
+    if(scalar(@split_class) != 1) {
+        $split_class[0] =~ /([<=>]{0,2})(.+)/;
+        my $oper = "$1";
+        my $class = "$2";
+    } else {
+        #this is so split version works if no class was given
+        $split_class[1] = $split_class[0];
+        $split_class[0] = undef;
+    }
+
+    $ret[4] = $split_slot[1];
+    $ret[1] = $class;
+    $ret[3] = $oper;
+    my @split_version = split_version($split_class[1], $split_class[0]);
+    print(Dumper \@split_class);
+}
+
+my @x = (
 '>=sys-libs/ncurses-5.9-r3:0/6',
 'app-admin/pass',
+'gcc',
 'acct-user/gnupg-pkcs11-scd-proxy',
 'sys-libs/libstdc++');
 
